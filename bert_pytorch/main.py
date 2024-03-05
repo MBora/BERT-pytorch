35ef7049f0ebcd2f1c992c073c8b53735aa13e88
Potato-Cracker <maheswarbora2@gmail.com>
2024-03-06 00:04:59 +0530

    reduce batch size

--- bert_pytorch/main.py
+++ bert_pytorch/main.py
@@ -24,7 +24,7 @@ def train():
     parser.add_argument("-s", "--seq_len", type=int, default=32, help="maximum sequence len")
     parser.add_argument("-d", "--dropout", type=float, default=0.0, help="dropout rate")
 
-    parser.add_argument("-b", "--batch_size", type=int, default=256, help="number of batch_size")
+    parser.add_argument("-b", "--batch_size", type=int, default=128, help="number of batch_size")
     parser.add_argument("-e", "--epochs", type=int, default=100, help="number of epochs")
     parser.add_argument("-w", "--num_workers", type=int, default=12, help="dataloader worker size")
 
