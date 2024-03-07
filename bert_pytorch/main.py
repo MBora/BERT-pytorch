b3740db29135d4727c963cfa8440421ad61ef1ab
Potato-Cracker <maheswarbora2@gmail.com>
2024-03-07 10:09:19 +0530

    change random seed

--- bert_pytorch/main.py
+++ bert_pytorch/main.py
@@ -9,7 +9,7 @@ from dataset.dataset import BERTDataset, BERTDatasetDual
 
 from vocab_builder import WordVocab
 
-torch.manual_seed(1)
+torch.manual_seed(42)
 
 def train():
     parser = argparse.ArgumentParser()
